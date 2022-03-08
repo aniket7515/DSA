5e07a317ad499794bd55686d299003ad3805b17b
Aniket Dhokane <80635510+aniket7515@users.noreply.github.com>
2022-03-08 22:03:51 +0530

    Toggle Case

---
+++ Strings and StringBuilders/ToggleCase.java
@@ -0,0 +1,29 @@
+package DSA.Strings;
+import java.util.Scanner;
+
+public class ToggleCase {
+
+    public static String toggleCase(String str){
+        StringBuilder sb= new StringBuilder(str);
+
+        for(int i=0;i<sb.length();i++){
+            char ch=sb.charAt(i);
+
+            if(ch>='a' && ch<='z'){
+                char uch=(char)('A'+ch-'a');
+                sb.setCharAt(i,uch);
+            }else if(ch>='A' && ch<='Z'){
+                char lch=(char)('a'+ch-'A');
+                sb.setCharAt(i,lch);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str=in.next();
+        System.out.println(toggleCase(str));
+
+    }
+}
