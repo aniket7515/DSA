b57e104326145c64683a8212ef8f7659dc6dc002
Aniket Dhokane <80635510+aniket7515@users.noreply.github.com>
2022-05-19 14:56:54 +0530

    Add files via upload

---
+++ ARRAYS/LeadersInArray.java
@@ -0,0 +1,21 @@
+package GFGSELFPACED.ARRAYS;
+
+public class LeadersInArray {
+    public static void main(String[] args) {
+        int[] arr={7,10,4,10,6,5,2};
+        leaders(arr);
+
+    }
+    public static void leaders(int[] arr){
+        int n=arr.length;
+        int curr_leader=arr[n-1];
+
+        System.out.println(curr_leader);
+        for(int i=n-2;i>=0;i--){
+            if(curr_leader<arr[i]){
+                curr_leader=arr[i];
+                System.out.println(curr_leader);
+            }
+        }
+    }
+}
