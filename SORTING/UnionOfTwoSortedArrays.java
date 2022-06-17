7bbd71a28f701f2c3e3d85828e8b99d16132408f
Aniket Dhokane <80635510+aniket7515@users.noreply.github.com>
2022-06-17 18:07:32 +0530

    Add files via upload

---
+++ SORTING/UnionOfTwoSortedArrays.java
@@ -0,0 +1,57 @@
+package GFGSELFPACED.SORTING;
+
+public class UnionOfTwoSortedArrays {
+    public static void main(String[] args) {
+        int[] a={2,10,20,20};
+        int[] b={3,20,20};
+
+        printUnion(a,b);
+
+    }
+    public static void printUnion(int[] a, int[] b){
+        int m=a.length;
+        int n=b.length;
+
+        int i=0;
+        int j=0;
+
+        while(i<m && j<n){
+            if(i>0 && a[i]==a[i-1]){
+                i++;
+                continue;
+            }
+            if(j>0 && b[j]==b[j-1]){
+                j++;
+                continue;
+            }
+            if(a[i]<b[j]){
+                System.out.print(a[i]+" ");
+                i++;
+            }
+            else if(a[i]>b[j]){
+                System.out.print(b[j]+" ");
+                j++;
+            }
+            else{
+                System.out.print(a[i]+" ");
+                i++;
+                j++;
+            }
+
+        }
+        while(i<m){
+            if(i>0 && a[i]!=a[i-1]){
+                System.out.print(a[i]+" ");
+                i++;
+            }
+
+        }
+        while(j<n){
+            if(j>0 && b[j]!=b[j-1]){
+                System.out.print(b[j]+" ");
+                j++;
+            }
+        }
+    }
+
+}
