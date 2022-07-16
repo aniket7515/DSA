4301f6a3f06ffa1a0bfc0eed9101f36b7ec2dcc0
Aniket Dhokane <80635510+aniket7515@users.noreply.github.com>
2022-07-16 11:17:11 +0530

    Update MajorityElements.java

--- ARRAYS/MajorityElements.java
+++ ARRAYS/MajorityElements.java
@@ -4,6 +4,7 @@ public class MajorityElements {
     public static void main(String[] args) {
         int[] arr={6,6,6,4,6,6,8,5};
         System.out.println(findMajority(arr));
+        System.out.println(findMajorityMoores(arr));
 
     }
     public static int findMajority(int[] arr){
@@ -17,6 +18,34 @@ public class MajorityElements {
             }
             if(count>n/2){
                 return i;
+            }
+        }return -1;
+    }
+    // Moores voting algorithm
+    // 1) we find the majority elelemt in first step and in step 2
+    // 2) we check the possible majority element
+    public static int findMajorityMoores(int[] arr){
+        int res=0,count=1;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[res]==arr[i]){
+                count++;
+            }else{
+                count--;
+            }
+            if(count==0){
+                res=i;
+                count=1;
+            }
+        }
+
+        count=0;
+        for(int i=0;i<n;i++){
+            if(arr[res]==arr[i]){
+                count++;
+            }
+            if(count>n/2){
+                return res;
             }
         }return -1;
     }
